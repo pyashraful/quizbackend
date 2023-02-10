c0b9b3c0efa32ea285da9e9c62e804c4e36125d8
pyashraful <pyashraful@gmail.com>
2023-02-10 18:58:38 +0600

    setpu husky

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import { foo } from '@src/hello.js';
+// import { foo } from '@src/hello.js';
 
 console.log('hello world');
 console.log('hello world');
 console.log('hello world');
 console.log('hello world');
 
-function this_is_husky() {
-  return 'hello world';
-}
+// function this_is_husky() {
+//   return 'hello world hello world hello world';
+// }
